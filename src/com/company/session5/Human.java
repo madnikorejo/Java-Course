f4a9b6d1ec2e037eca3f3dd3e9800db62ab4a91d
Abdul Basit3 <abdul.basit3@ibm.com>
2022-07-31 22:59:48 +0500

    [Commit 02] Adding Tax to Amna's Bank Balance

--- src/com/company/session5/Human.java
+++ src/com/company/session5/Human.java
@@ -5,8 +5,17 @@ public class Human {
     public int age = 25;
     private int CNIC = 35345353;
     private int bankBalance = 10000000;
+    private int tax;
 
     public int getCNIC() {
         return CNIC;
+    }
+
+    public int calculateTax() {
+        if (bankBalance >= 10000000) {
+            tax = 500000;
+        }
+
+        return tax;
     }
 }
